3ff8f4b350b89066264271b0b97d74d0a8949eb7
Filip Civljak <filipcivljak007@gmail.com>
2026-02-24 12:24:21 +0100

    Fix site URL to include www

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://absmach.eu",
+  site: "https://www.absmach.eu",
   output: "static",
   integrations: [
     tailwind(),
